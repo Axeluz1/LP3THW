7361e4d7d9752b03eb8b184c46c54e95f74a0f39
Axeluz1 <130583819+Axeluz1@users.noreply.github.com>
2023-04-12 11:31:57 -0600

    Add files via upload

---
+++ ex29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("hay muchos gatos")
+
+if people > cats:
+    print("no hay muchos gatos")
+
+if people < dogs:
+    print("hay muchos perros")
+
+if people > dogs:
+    print("el mundo esta seco")
+
+dogs += 5
+
+if people >= dogs:
+    print("se necesita equilibrio")
+
+if people <= dogs:
+    print("people are less than or equal to dogs")
+
+if people == dogs:
+    print("personas son perros")
